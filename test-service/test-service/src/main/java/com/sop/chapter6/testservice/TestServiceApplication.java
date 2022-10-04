442c77501a06ea751cfb928eea039b89f59a7f4b
mingchanoknan <73680838+mingchanoknan@users.noreply.github.com>
2022-10-04 10:23:45 +0700

    finish

---
+++ test-service/test-service/src/main/java/com/sop/chapter6/testservice/TestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sop.chapter6.testservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestServiceApplication.class, args);
+	}
+
+}
